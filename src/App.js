c6b416a7281a7e98475b679b38ea68aeff1f3a81
Kunal Kumar <kunal34255@gmail.com>
2023-08-01 16:59:17 +0530

    working directry clean

--- src/App.js
+++ src/App.js
@@ -1,85 +1,9 @@
-import Header from './components/Header';
-import './App.css';
-import { createBrowserRouter, RouterProvider, Link ,BrowserRouter} from "react-router-dom";
-import ProductList from './components/ProductList';
-import ProductDetaile from './components/ProductDetaile';
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-        <ProductList></ProductList>
-    ),
-  },
-  {
-    path: "/product-detaile/:id",
-    element: (
-        <ProductDetaile></ProductDetaile>
-    ),
-  },
-])
+
 function App() {
   return (
     <div className='App'>
-      <BrowserRouter>
-      <Header></Header>
-     {/* <RouterProvider router={router}/> */}
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     <div>heoll</div>
-     </BrowserRouter>
+      hello world
+      
     </div>
   );
 }
--- src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import { productReducer,singleProductReducer } from "./productReducer";
-
-const reducers = combineReducers({
-    allProducts:productReducer,
-    oneProduct:singleProductReducer,
-});
-
-export default reducers;
